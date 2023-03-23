f5554c44fc88de24b55a29534759ed90581b1426
René Wang <52880665+RiverTwilight@users.noreply.github.com>
2023-03-23 22:14:31 +0800

    add perceptron

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Digit Detection with Deep Learning
 
-This is a full example of how to detect hand-writting number with deep learning.
+This is a full example of how to detect hand-writting number with deep learning. The example is based on the [MNIST dataset](http://yann.lecun.com/exdb/mnist/). The dataset contains 60,000 training images and 10,000 testing images. Each image is a 28x28 grayscale image of a hand-written digit. The goal is to train a model to correctly classify the digit in the image.
 
 ## Run
 
---
+++ perceptron.py
@@ -0,0 +1,28 @@
+# Perceptron algorithm
+# It is a simplified model of biological neurons and is the building block of artificial neural networks.
+
+import numpy as np
+
+
+# Only when both x1 and x2 is 1, the output is 1.
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2 # When meet the requirement, it should be 1 now.
+    if tmp <= theta: # Note this equal to 'tmp - theta <= 0'
+        return 0
+    elif tmp > theta:
+        return 1
+
+# Use numpy
+def AND_2(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = 0.7
+    tmp = np.sum(x * w) - b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print(AND_2(1, 1))
+print(AND_2(1, 0))
